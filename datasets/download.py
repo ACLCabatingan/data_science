2ae6f8154c95b4d3d482cc11a3f627d66d9628f6
acabatingan <acabatingan@savvysherpa.com>
2020-10-15 12:47:55 +0000

    Fix bug on where data is dumped

--- datasets/download.py
+++ datasets/download.py
@@ -3,12 +3,18 @@ import time
 from zipfile import ZipFile
 from kaggle.api.kaggle_api_extended import KaggleApi
 
-#TODO: 
+
+
+data_path = os.path.expanduser('~')+'/data_science/datasets/data/'
 
 def dnld_kaggle_data(dataset, filename, delete_zip=True):
     #MVP function to download data from kaggle
-    
+      
     t1  = time.time() 
+      
+    if os.path.isfile(data_path+filename):
+        print('Data already exist!')
+        return
     
     #connect
     api = KaggleApi()
@@ -29,7 +35,7 @@ def dnld_kaggle_data(dataset, filename, delete_zip=True):
         if delete_zip:
             os.remove(filename+'.zip')
             
-    os.replace(os.getcwd()+'/'+filename, os.path.expanduser('~')+'/data_science/datasets/data/'+filename)
+    os.replace(os.getcwd()+'/'+filename, data_path+filename)
     t2 = time.time()
     
-    print(f'File ready! It took {round(t2-t1, 1)} seconds.')
+    print(f'Data ready! It took {round(t2-t1, 1)} seconds.')
